ef382dfcef0b991f97393a3d597fccc639bd7fa7
Colin Knox <colin@platphormcorp.com>
2017-02-26 22:51:27 -0600

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,8 @@ module Jeeves
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+
+    config.time_zone = 'Central Time (US & Canada)'
+
   end
 end
