70af6038f7d17ab1b20e2f8477b45c26ea993478
Colin Knox <colin@platphormcorp.com>
2017-03-02 23:21:19 -0600

    improved volume control, and rubocop styling suggestions

--- app/lib/speech_engine.rb
+++ app/lib/speech_engine.rb
@@ -2,31 +2,29 @@ class SpeechEngine
   OS_LOOKUP = {
     'armv7l-linux-eabihf' => 'raspberrypi',
     'x86_64-darwin16' => 'osx',
-  }
+  }.freeze
 
-  @@volume = 100
+  @volume = 100
 
   class << self
-
     def say(sentence, options = {})
-      if options[:volume] && options[:volume] != @@volume
-        volume = options[:volume]
+      if options[:volume] && options[:volume] != @volume
+        self.volume = options[:volume]
       end
 
       run_command speech_command(sentence)
     end
 
     def volume
-      @@volume
+      @volume
     end
 
     # this command can take a while, be aware that it's possible
     # for the engine to begin speaking before volume is fully set
     def volume=(new_volume)
-      if @@volume != new_volume
-        @@volume = new_volume
-        run_command volume_command(new_volume)
-      end
+      return if @volume == new_volume
+      @volume = new_volume
+      run_command volume_command(new_volume)
     end
 
     private
@@ -34,9 +32,9 @@ class SpeechEngine
     def speech_command(sentence = 'thing happened')
       case OS_LOOKUP[RUBY_PLATFORM]
       when 'raspberrypi'
-        %Q{echo "#{sentence}" | festival --tts}
+        %(echo "#{sentence}" | festival --tts)
       when 'osx'
-        %Q{say "#{sentence}"}
+        %(say "#{sentence}")
       end
     end
 
@@ -44,11 +42,11 @@ class SpeechEngine
       scale_to_range = ->(value, range) { range[((range.size - 1) * (value.to_i.clamp(0, 100) / 100.0)).round] }
       case OS_LOOKUP[RUBY_PLATFORM]
       when 'raspberrypi'
-        scaled_volume = scale_to_range.call(new_volume, (-10239..400).to_a)
-        %Q{amixer set PCM -- #{scaled_volume}}
+        scaled_volume = scale_to_range.call(new_volume, (-10_239..400).to_a)
+        %(amixer set PCM -- #{scaled_volume})
       when 'osx'
-        scaled_volume = scale_to_range.call(new_volume, (0..7).step(0.1).to_a.map{|x| x.round(1)})
-        %Q{osascript -e "set Volume #{scaled_volume}"}
+        scaled_volume = scale_to_range.call(new_volume, (0..7).step(0.1).to_a.map { |x| x.round(1) })
+        %(osascript -e "set Volume #{scaled_volume}")
       end
     end
 
