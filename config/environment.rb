4bf81ab91ffbd76e84ba1c8c5d938d8a0ecd934a
Colin Knox <colin@platphormcorp.com>
2017-03-02 23:33:58 -0600

    display version number in footer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+APP_VERSION = `git describe --always`.freeze unless defined? APP_VERSION
+
 # Initialize the Rails application.
 Rails.application.initialize!
