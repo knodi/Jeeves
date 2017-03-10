27c8bc8037a0d1418b396932305cb02a21b2b1e0
Colin Knox <colin@platphormcorp.com>
2017-03-09 23:52:36 -0600

    rudimentary styling

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,12 @@
 # Load the Rails application.
 require_relative 'application'
 
-#APP_VERSION = `git describe --always`.freeze unless defined? APP_VERSION
-APP_VERSION = Rails.configuration.root.to_s.split('/').last
+# APP_VERSION = `git describe --always`.freeze unless defined? APP_VERSION
+APP_VERSION = if Rails.env.development?
+                'development'
+              else
+                Rails.configuration.root.to_s.split('/').last
+              end
 
 # Initialize the Rails application.
 Rails.application.initialize!
