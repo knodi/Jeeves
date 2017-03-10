a8a8f6596db2cb230e03a86ef5ee60f7cecb158c
Colin Knox <colin@platphormcorp.com>
2017-03-10 00:07:37 -0600

    lua luup docs and http auth example

--- mcv_script.lua
+++ mcv_script.lua
@@ -1,3 +1,3 @@
 -- generic MCV lua script
-local status, result = luup.inet.wget("http://raspberrypi.local:3000/events/new?d=back%20door&l=back%20door%20open", 5)
+local status, result = luup.inet.wget("http://raspberrypi.local:3000/events/new?d=back%20door&l=back%20door%20open", 5, 'cknox', 'testing123')
 
